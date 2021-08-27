0d99718c9f28202c830874f601b955fd859a7848
Yang-Zhao <823504052@qq.com>
2021-08-27 22:35:24 +0800

    高德地图纠偏

--- questionnaire_planet/src/views/Test.vue
+++ questionnaire_planet/src/views/Test.vue
@@ -64,65 +64,35 @@ export default {
       var GPS = {
 
         PI: 3.14159265358979324,
-
         x_pi: 3.14159265358979324 * 3000.0 / 180.0,
-
         delta: function (lat, lon) {
-
           var a = 6378245.0; //  a: 卫星椭球坐标投影到平面地图坐标系的投影因子。
-
           var ee = 0.00669342162296594323; //  ee: 椭球的偏心率。
-
           var dLat = this.transformLat(lon - 105.0, lat - 35.0);
-
           var dLon = this.transformLon(lon - 105.0, lat - 35.0);
-
           var radLat = lat / 180.0 * this.PI;
-
           var magic = Math.sin(radLat);
-
           magic = 1 - ee * magic * magic;
-
           var sqrtMagic = Math.sqrt(magic);
-
           dLat = (dLat * 180.0) / ((a * (1 - ee)) / (magic * sqrtMagic) * this.PI);
-
           dLon = (dLon * 180.0) / (a / sqrtMagic * Math.cos(radLat) * this.PI);
-
           return {
-
             'lat': dLat,
-
             'lon': dLon
-
           };
-
         },
-
         //WGS-84 to GCJ-02
-
         gcj_encrypt: function (wgsLat, wgsLon) {
-
           if (this.outOfChina(wgsLat, wgsLon))
-
             return {
-
               'lat': wgsLat,
-
               'lon': wgsLon
-
             };
 
-
-
           var d = this.delta(wgsLat, wgsLon);
-
           return {
-
             'lat': wgsLat + d.lat,
-
             'lon': wgsLon + d.lon
-
           };
 
         },
